c5ff214da2ec7e1acfcbab2233b393918636b5b0
ahmedelsalamony <ahmedabdelhameed79@gmail.com>
2024-12-11 14:16:17 +0200

    screen utils configuration

--- lib/store_app.dart
+++ lib/store_app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:store/core/app/connectivity_controller.dart';
 
 class StoreApp extends StatelessWidget {
@@ -11,13 +12,17 @@ class StoreApp extends StatelessWidget {
       valueListenable: ConnectivityController.instance.isOnline,
       builder: (context, value, child) {
         if (value) {
-          return MaterialApp(
-            title: 'Flutter Demo',
-            theme: ThemeData(
-              colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-              useMaterial3: true,
+          return ScreenUtilInit(
+            designSize: const Size(360, 690),
+            minTextAdapt: true,
+            child: MaterialApp(
+              title: 'Flutter Demo',
+              theme: ThemeData(
+                colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+                useMaterial3: true,
+              ),
+              home: const Scaffold(body: Center(child: Text("FlutterApp"))),
             ),
-            home: const Scaffold(body: Center(child: Text("FlutterApp"))),
           );
         } else {
           return const Scaffold(body: Center(child: Text("No Internet")));
